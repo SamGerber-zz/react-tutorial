6eb4cab24764580e8ad31523f174b139ff602b0e
SamGerber <gerbyxvi@gmail.com>
2016-02-16 00:41:35 -0800

    Initial commit

---
+++ frontend/tutorial.jsx
@@ -0,0 +1,9 @@
+var React = require('react'),
+  ReactDOM = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function(){
+  React.render(
+    <div>Hello, world!</div>,
+      document.body
+    );
+});
